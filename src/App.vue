e776b82ccef48b4a2d1057fa03ba03d52b0fde0e
Vasco Neves <57102041+vasconevesxd@users.noreply.github.com>
2024-11-01 19:23:45 +0000

    Setting Up Vue Router and Navigation with Router Link

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Hi</h1>
+  <main>
+    <RouterView />
+  </main>
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <RouterLink to="/projects">Go to Projects</RouterLink>
+  </div>
+</template>
