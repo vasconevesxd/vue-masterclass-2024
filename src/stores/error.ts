a528016f89aff376a5e3a8d3113f72f76b08e76b
Vasco Neves <57102041+vasconevesxd@users.noreply.github.com>
2024-12-11 21:54:18 +0000

    Display different types of errors

--- src/stores/error.ts
+++ src/stores/error.ts
@@ -1,4 +1,4 @@
-import type { CustomError } from '@/types/Error'
+import type { CustomError, ExtendedPostgrestError } from '@/types/Error'
 import type { PostgrestError } from '@supabase/supabase-js'
 
 export const useErrorStore = defineStore('error-store', () => {
@@ -8,17 +8,17 @@ export const useErrorStore = defineStore('error-store', () => {
     error,
     customCode,
   }: {
-    error: string | PostgrestError
-    customCode: number
+    error: string | PostgrestError | Error
+    customCode?: number
   }) => {
-    if (typeof error === 'string') {
-      activeError.value = Error(error)
-      activeError.value.customCode = customCode
+    if (typeof error === 'string' || error instanceof Error) {
+      activeError.value = typeof error === 'string' ? Error(error) : error
+      activeError.value.customCode = customCode || 500
       return
     }
 
     activeError.value = error
-    activeError.value.statusCode = customCode
+    activeError.value.statusCode = customCode || 500
   }
 
   return {
